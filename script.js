21fc1e29d7032e80d4a45fbac651b9b251c22b3b
FacundoChacon <chaconfacu2006@gmail.com>
2026-02-20 11:00:46 -0300

    se ha añadido un maximo de caracteres en contacto y se ha añadido el video de fondo

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
 function scrollToContact() {
     document.getElementById("contacto").scrollIntoView({
-      behavior: "smooth"
+        behavior: "smooth"
     });
-  }
+    }
 
-  const form = document.getElementById("contactForm");
-  const toast = document.getElementById("toast");
+    const form = document.getElementById("contactForm");
+    const toast = document.getElementById("toast");
 
-  form.addEventListener("submit", function(e) {
+    form.addEventListener("submit", function(e) {
     e.preventDefault();
 
     toast.classList.add("show");
 
     setTimeout(() => {
-      toast.classList.remove("show");
-      form.reset();
+        toast.classList.remove("show");
+        form.reset();
     }, 2500);
-  });
+    });
+
+    const textarea = document.getElementById("mensaje");
+    const contador = document.getElementById("caracteresRestantes");
+    const maxCaracteres = 200;
+    
+    textarea.addEventListener("input", function() {
+    
+      // 🔢 Calcular caracteres restantes
+    const longitudActual = this.value.length;
+    const restantes = maxCaracteres - longitudActual;
+
+    contador.textContent = restantes;
+    
+      // 📈 Auto expandir hacia abajo
+    this.style.height = "auto";
+    this.style.height = this.scrollHeight + "px";
+    
+      // 📜 Activar scroll si supera altura máxima
+    if (this.scrollHeight > 250) {
+        this.style.overflowY = "scroll";
+    } else {
+        this.style.overflowY = "hidden";
+    }
+    });
+    
